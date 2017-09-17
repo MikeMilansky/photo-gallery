54f07b9a3ac29607da661206cf7b455f8dd1b29c
Nikita Reshetnikov <mikemilansky@gmail.com>
2017-09-17 23:06:58 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'semi': [2, "always"],
+    "comma-dangle": ["error", "never"],
+    "one-var": [
+      2,
+      {
+        "const": "never"
+      }
+    ],
+    "space-before-function-paren": [
+      2,
+      {
+        "anonymous": "always",
+        "named": "ignore"
+      }
+    ]
+  }
+}
